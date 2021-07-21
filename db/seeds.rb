96ed28fd57c354dd9cc53542895791ff981938b2
TashaLapitanova <natalia@farmy.ch>
2021-07-21 13:23:12 +0200

    add test data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+d = Date.today
+
+User.create({username: "Cookie Monster", password: "I love cookies", age: 63})
+
+1.upto(10) do |i|
+  Expense.create({title: "Expense No.#{i}", amount: (i + i*10), date: (d-i).to_s, user_id: 2})
+end
+
+1.upto(20) do |i|
+  Income.create({title: "Income No.#{i}", amount: (i + i*100), date: (d-i).to_s, user_id: 2})
+end
